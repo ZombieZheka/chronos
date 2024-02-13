51858adfc49f4634c8655110d7c7235784f28a45
Yevhen Kovalov <kovalov.yevhen.07@gmaol.com>
2024-02-13 11:26:12 +0200

    create backend skeleton

---
+++ backend/migrations/20240212230657-create-event.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      note: {
+        type: Sequelize.TEXT
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      duration: {
+        type: Sequelize.TIME
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  }
+};
