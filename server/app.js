626ef5fcda3571bcf7e1bb38047a418c7cae6b20
Yevhen Kovalov <kovalov.yevhen.07@gmaol.com>
2024-03-04 14:03:10 +0200

    add docker

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,21 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// dotenv setup
-const dotenv = require("dotenv");
-dotenv.config();
-const root = path.resolve(__dirname);
-process.env.MODELS = path.join(root, "models");
-process.env.ROUTES = path.join(root, "routes");
-process.env.CONTROLLERS = path.join(root, "controllers");
-
-// database setup
-const { sequelize } = require("./models/index");
-sequelize.sync({force: true});
-
-// routers setup
-const router = require('./routes/index.router');
-app.use('/', router);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
